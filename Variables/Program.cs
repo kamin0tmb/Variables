757e4964c21ae490142b2cd32e2586654dd87515
kamin0 <jm187@yandex.ru>
2022-04-01 20:47:10 +0300

    Светофор

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -4,7 +4,7 @@ namespace Variables
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             string MyName = "Johnny";
             byte MyAge = 27;
@@ -14,9 +14,18 @@ namespace Variables
             Console.WriteLine("I'm {0} years old", MyAge);
             Console.WriteLine("Do I have a pet?" + APet);
             Console.WriteLine("My shoes size is " + Shoes);
-            Console.ReadKey();
             Console.WriteLine("IntMinValue {0}", int.MinValue);
             Console.WriteLine("IntMaxValue {0}", int.MaxValue);
+            Semaphore MyFavoriteCollor;
+            MyFavoriteCollor = Semaphore.Green;
+            Console.WriteLine(MyFavoriteCollor);
         }
+            enum Semaphore
+        {   
+            Red = 100,
+            Yellow = 200,
+            Green = 300
+        }
+        
     }
 }
