3a050eda0a45a3ada7ceec8a3d620986b9bd49f9
kamin0 <jm187@yandex.ru>
2022-04-01 20:23:35 +0300

    Добавлены переменные string, byte, bool, dpuble.

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -7,8 +7,15 @@ namespace Variables
         static void Main(string[] args)
         {
             string MyName = "Johnny";
-            Console.WriteLine(MyName);
+            byte MyAge = 27;
+            bool APet = false;
+            double Shoes = 37.1;
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("I'm {0} years old", MyAge);
+            Console.WriteLine("Do I have a pet?" + APet);
+            Console.WriteLine("My shoes size is " + Shoes);
             Console.ReadKey();
+
         }
     }
 }
